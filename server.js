b5157dc3106e498335f682b68f63ca5f119a5fd2
SokhadomKhorn <tpcoea@gmail.com>
2026-03-23 18:22:29 +0700

    Refactor server.js to use express and add routes
    Replaced dotenv configuration with express setup and added API routes.

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
-require("dotenv").config();
-
-const app = require("./src/app");
-const connectDB = require("./src/config/db");
-
+const express = require("express");
+const app = express();
 const PORT = process.env.PORT || 3000;
 
-connectDB();
+app.get("/", (req, res) => {
+  res.json({ message: "API is running!" });
+});
+
+app.get("/search", (req, res) => {
+  res.json({ message: "INSIDE SEARCH API" });
+});
+
+app.get("/view", (req, res) => {
+  res.json({ message: "INSIDE VIEW API" });
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
